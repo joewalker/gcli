263e652468c4b76a005c8b62906fabf3c555acc5
Joe Walker <jwalker@mozilla.com>
2011-08-05 16:30:14 +0100

    Bug 672313 - Mini-require should be pulled out of GCLI and made a submodule so it can be shared with Source Map This splits prefix-gcli out into console.js and tweaks mini-require so it can be used both in a browser and in a jsm. Also the makefile is updated for the new separation, and also to workaround a problem with uglify on windows where it fails to compress files

--- build/mini_require.js
+++ build/mini_require.js
@@ -4,20 +4,12 @@
  * http://opensource.org/licenses/BSD-3-Clause
  */
 
-(function() {
-
-// There are 2 virtually identical copies of this code:
-// - $GCLI_HOME/build/prefix-gcli.jsm
-// - $GCLI_HOME/build/mini_require.js
-// They should both be kept in sync
-
-var debugDependencies = false;
 
 /**
  * Define a module along with a payload.
- * @param {string} moduleName Name for the payload
- * @param {ignored} deps Ignored. For compatibility with CommonJS AMD Spec
- * @param {function} payload Function with (require, exports, module) params
+ * @param moduleName Name for the payload
+ * @param deps Ignored. For compatibility with CommonJS AMD Spec
+ * @param payload Function with (require, exports, module) params
  */
 function define(moduleName, deps, payload) {
   if (typeof moduleName != "string") {
@@ -30,7 +22,7 @@ function define(moduleName, deps, payload) {
     payload = deps;
   }
 
-  if (debugDependencies) {
+  if (define.debugDependencies) {
     console.log("define: " + moduleName + " -> " + payload.toString()
         .slice(0, 40).replace(/\n/, '\\n').replace(/\r/, '\\r') + "...");
   }
@@ -39,125 +31,130 @@ function define(moduleName, deps, payload) {
     console.error(this.depth + " Error: Redefining module: " + moduleName);
   }
   define.modules[moduleName] = payload;
-};
+}
 
 /**
  * The global store of un-instantiated modules
  */
 define.modules = {};
 
+/**
+ * Should we console.log on module definition/instantiation/requirement?
+ */
+define.debugDependencies = false;
+
 
 /**
- * We invoke require() in the context of a Domain so we can have multiple
- * sets of modules running separate from each other.
- * This contrasts with JSMs which are singletons, Domains allows us to
- * optionally load a CommonJS module twice with separate data each time.
- * Perhaps you want 2 command lines with a different set of commands in each,
- * for example.
+ * Self executing function in which Domain is defined, and attached to define
  */
-function Domain() {
-  this.modules = {};
+(function() {
+  /**
+   * We invoke require() in the context of a Domain so we can have multiple
+   * sets of modules running separate from each other.
+   * This contrasts with JSMs which are singletons, Domains allows us to
+   * optionally load a CommonJS module twice with separate data each time.
+   * Perhaps you want 2 command lines with a different set of commands in each,
+   * for example.
+   */
+  function Domain() {
+    this.modules = {};
 
-  if (debugDependencies) {
-    this.depth = "";
-  }
-}
-
-/**
- * Lookup module names and resolve them by calling the definition function if
- * needed.
- * There are 2 ways to call this, either with an array of dependencies and a
- * callback to call when the dependencies are found (which can happen
- * asynchronously in an in-page context) or with a single string an no callback
- * where the dependency is resolved synchronously and returned.
- * The API is designed to be compatible with the CommonJS AMD spec and
- * RequireJS.
- * @param {string[]|string} deps A name, or names for the payload
- * @param {function|undefined} callback Function to call when the dependencies
- * are resolved
- * @return {undefined|object} The module required or undefined for
- * array/callback method
- */
-Domain.prototype.require = function(deps, callback) {
-  if (Array.isArray(deps)) {
-    var params = deps.map(function(dep) {
-      return this.lookup(dep);
-    }, this);
-    if (callback) {
-      callback.apply(null, params);
+    if (define.debugDependencies) {
+      this.depth = "";
     }
-    return undefined;
   }
-  else {
-    return this.lookup(deps);
-  }
-};
 
-/**
- * Lookup module names and resolve them by calling the definition function if
- * needed.
- * @param {string} moduleName A name for the payload to lookup
- * @return {object} The module specified by aModuleName or null if not found.
- */
-Domain.prototype.lookup = function(moduleName) {
-  if (moduleName in this.modules) {
-    var module = this.modules[moduleName];
-    if (debugDependencies) {
-      console.log(this.depth + " Using module: " + moduleName);
+  /**
+   * Lookup module names and resolve them by calling the definition function if
+   * needed.
+   * There are 2 ways to call this, either with an array of dependencies and a
+   * callback to call when the dependencies are found (which can happen
+   * asynchronously in an in-page context) or with a single string an no
+   * callback where the dependency is resolved synchronously and returned.
+   * The API is designed to be compatible with the CommonJS AMD spec and
+   * RequireJS.
+   * @param deps A name, or array of names for the payload
+   * @param callback Function to call when the dependencies are resolved
+   * @return The module required or undefined for array/callback method
+   */
+  Domain.prototype.require = function(deps, callback) {
+    if (Array.isArray(deps)) {
+      var params = deps.map(function(dep) {
+        return this.lookup(dep);
+      }, this);
+      if (callback) {
+        callback.apply(null, params);
+      }
+      return undefined;
     }
+    else {
+      return this.lookup(deps);
+    }
+  };
+
+  /**
+   * Lookup module names and resolve them by calling the definition function if
+   * needed.
+   * @param moduleName A name for the payload to lookup
+   * @return The module specified by aModuleName or null if not found
+   */
+  Domain.prototype.lookup = function(moduleName) {
+    if (moduleName in this.modules) {
+      var module = this.modules[moduleName];
+      if (define.debugDependencies) {
+        console.log(this.depth + " Using module: " + moduleName);
+      }
+      return module;
+    }
+
+    if (!(moduleName in define.modules)) {
+      console.error(this.depth + " Missing module: " + moduleName);
+      return null;
+    }
+
+    var module = define.modules[moduleName];
+
+    if (define.debugDependencies) {
+      console.log(this.depth + " Compiling module: " + moduleName);
+    }
+
+    if (typeof module == "function") {
+      if (define.debugDependencies) {
+        this.depth += ".";
+      }
+
+      var exports = {};
+      try {
+        module(this.require.bind(this), exports, { id: moduleName, uri: "" });
+      }
+      catch (ex) {
+        console.error("Error using module: " + moduleName, ex);
+        throw ex;
+      }
+      module = exports;
+
+      if (define.debugDependencies) {
+        this.depth = this.depth.slice(0, -1);
+      }
+    }
+
+    // cache the resulting module object for next time
+    this.modules[moduleName] = module;
+
     return module;
-  }
+  };
 
-  if (!(moduleName in define.modules)) {
-    console.error(this.depth + " Missing module: " + moduleName);
-    return null;
-  }
-
-  var module = define.modules[moduleName];
-
-  if (debugDependencies) {
-    console.log(this.depth + " Compiling module: " + moduleName);
-  }
-
-  if (typeof module == "function") {
-    if (debugDependencies) {
-      this.depth += ".";
-    }
-
-    var exports = {};
-    try {
-      module(this.require.bind(this), exports, { id: moduleName, uri: "" });
-    }
-    catch (ex) {
-      console.error("Error using module: " + moduleName, ex);
-      throw ex;
-    }
-    module = exports;
-
-    if (debugDependencies) {
-      this.depth = this.depth.slice(0, -1);
-    }
-  }
-
-  // cache the resulting module object for next time
-  this.modules[moduleName] = module;
-
-  return module;
-};
-
-/**
- * Expose the Domain constructor and a global domain (on the define function
- * to avoid exporting more than we need. This is a common pattern with require
- * systems)
- */
-define.Domain = Domain;
-define.globalDomain = new Domain();
+  /**
+   * Expose the Domain constructor and a global domain (on the define function
+   * to avoid exporting more than we need. This is a common pattern with
+   * require systems)
+   */
+  define.Domain = Domain;
+  define.globalDomain = new Domain();
+})();
 
 /**
  * Expose a default require function which is the require of the global
  * sandbox to make it easy to use.
  */
-window.define = define;
-window.require = define.globalDomain.require.bind(define.globalDomain);
-
-})();
+var require = define.globalDomain.require.bind(define.globalDomain);
