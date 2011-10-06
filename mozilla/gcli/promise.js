8c6dc79a27d06e37dc312030b1b153589ef72ec0
Joe Walker <jwalker@mozilla.com>
2011-10-06 22:42:29 +0100

    Bug 692424 (promise): Now Firefox has a promise, we don't need to export ours, we just use theirs.

---
+++ mozilla/gcli/promise.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2009-2011 Mozilla Foundation and contributors
+ * Licensed under the New BSD license. See LICENSE.txt or:
+ * http://opensource.org/licenses/BSD-3-Clause
+ */
+
+define(function(require, exports, module) {
+
+  Components.utils.import("resource:///modules/devtools/Promise.jsm");
+  exports.Promise = Promise;
+
+});
