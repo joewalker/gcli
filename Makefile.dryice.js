88163e4542afd6f194b76ca6b24a1be14bdda56c
Joe Walker <jwalker@mozilla.com>
2011-11-29 14:54:43 +0000

    Bug 703062 (tests): Allow tests to be run under node

--- Makefile.dryice.js
+++ Makefile.dryice.js
@@ -22,6 +22,9 @@ function main() {
   else if (args[2] === 'firefox') {
     buildFirefox(args[3]);
   }
+  else if (args[2] === 'test') {
+    test();
+  }
   else {
     console.error('Error: Unknown target: \'' + args[2] + '\'');
     process.exit(1);
@@ -369,6 +372,35 @@ function createUndefineFunction(project) {
   });
   undefine += '}\n';
   return { value: undefine };
+}
+
+/**
+ * Run the test suite inside node
+ */
+function test() {
+  var requirejs = require('./scripts/r.js');
+
+  requirejs.config({
+    nodeRequire: require,
+    paths: { 'text': 'scripts/text', 'i18n': 'scripts/i18n' },
+    packagePaths: {
+      'lib': [
+        { name: 'gcli', main: 'index', lib: '.' },
+        { name: 'test', main: 'index', lib: '.' },
+        { name: 'gclitest', main: 'index', lib: '.' },
+        { name: 'demo', main: 'index', lib: '.' }
+      ]
+    }
+  });
+
+  // A minimum fake dom to get us through the JS tests
+  var document = { title: 'Fake DOM' };
+  requirejs('gcli/types/javascript').setGlobalObject({
+    window: { document: document },
+    document: document
+  });
+
+  requirejs('gclitest/suite');
 }
 
 // Now everything is defined properly, start working
