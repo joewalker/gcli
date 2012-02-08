joewalker/gcli

commit d73fde862a249a9ad6e8605563c689088b106535

Bug 720702 (noargfetch): Support tooltip option in field Allows us to use BlankField in place of fields like StringField that are not really helpful except in the dialog case.

lib/gcli/ui/field.js
@@ -127,10 +127,21 @@ Field.prototype.onInputChange = function(ev) {
 Field.claim = function() {
   throw new Error('Field should not be used directly');
 };
-Field.MATCH = 5;
-Field.DEFAULT_MATCH = 4;
-Field.IF_NOTHING_BETTER = 1;
-Field.NO_MATCH = 0;
+
+/**
+ * About minimalism - If we're producing a dialog, we want a field for every
+ * parameter. If we're providing a quick tooltip, we only want a field when
+ * it's really going to help.
+ * The getField() function takes an option of 'tooltip: true'. Fields are
+ * expected to reply with a TOOLTIP_* constant if they should be shown in the
+ * tooltip case.
+ */
+Field.TOOLTIP_MATCH = 5;   // A best match, that works for a tooltip
+Field.TOOLTIP_DEFAULT = 4; // A default match that should show in a tooltip
+Field.MATCH = 3;           // Match, but ignorable if we're being minimalist
+Field.DEFAULT = 2;         // This is a default (non-minimalist) match
+Field.BASIC = 1;           // OK in an emergency. i.e. assume Strings
+Field.NO_MATCH = 0;        // This field can't help with the given type
 
 
 /**
@@ -175,6 +186,10 @@ function getField(type, options) {
   if (!ctor) {
     console.error('Unknown field type ', type, ' in ', fieldCtors);
     throw new Error('Can\'t find field for ' + type);
+  }
+
+  if (options.tooltip && highestClaim < Field.TOOLTIP_DEFAULT) {
+    return new BlankField(type, options);
   }
 
   return new ctor(type, options);
@@ -370,7 +385,10 @@ function SelectionField(type, options) {
 SelectionField.prototype = Object.create(Field.prototype);
 
 SelectionField.claim = function(type) {
-  return type instanceof SelectionType ? Field.DEFAULT_MATCH : Field.NO_MATCH;
+  if (type instanceof BooleanType) {
+    return Field.BASIC;
+  }
+  return type instanceof SelectionType ? Field.TOOLTIP_DEFAULT : Field.NO_MATCH;
 };
 
 SelectionField.prototype.destroy = function() {
@@ -445,7 +463,7 @@ function JavascriptField(type, options) {
 JavascriptField.prototype = Object.create(Field.prototype);
 
 JavascriptField.claim = function(type) {
-  return type instanceof JavascriptType ? Field.MATCH : Field.NO_MATCH;
+  return type instanceof JavascriptType ? Field.TOOLTIP_MATCH : Field.NO_MATCH;
 };
 
 JavascriptField.prototype.destroy = function() {

commit 5e68f8a95e2a0aad808a77cbae1c64d895a680f2

Bug 725234 (refactor13a): Fix Field matching constant Broken by d73fde862a249a9ad6e8605563c689088b106535

lib/gcli/ui/field.js
@@ -241,7 +241,7 @@ StringField.prototype.getConversion = function() {
 };
 
 StringField.claim = function(type) {
-  return type instanceof StringType ? Field.MATCH : Field.IF_NOTHING_BETTER;
+  return type instanceof StringType ? Field.MATCH : Field.BASIC;
 };
 
 exports.StringField = StringField;