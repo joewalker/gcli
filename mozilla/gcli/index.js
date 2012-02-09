340539d0e98c4eb08b5ddd4bc51642cb041d29b8
Joe Walker <jwalker@mozilla.com>
2012-02-09 10:46:25 +0000

    Bug 725234 (refactor13a): Catchup moz index with main index

--- mozilla/gcli/index.js
+++ mozilla/gcli/index.js
@@ -56,10 +56,15 @@ define(function(require, exports, module) {
   require('gcli/types/javascript').startup();
   require('gcli/types/node').startup();
   require('gcli/types/resource').startup();
+  require('gcli/types/setting').startup();
   require('gcli/types/selection').startup();
 
+  require('gcli/settings').startup();
   require('gcli/cli').startup();
+  require('gcli/ui/intro').startup();
+
   require('gcli/commands/help').startup();
+  require('gcli/commands/pref').startup();
 
   var Requisition = require('gcli/cli').Requisition;
   var Console = require('gcli/ui/console').Console;
