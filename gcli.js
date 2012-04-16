5fa085c01ad1ed00ff7ea0af77d2812cadf2e5a7
Joe Walker <jwalker@mozilla.com>
2012-04-16 15:47:33 +0100

    Bug 745741 (coverage): Move commands to 'commands' directory - Saves mixing things up too much - It's how it's done elsewhere in GCLI - It helps with adding coverage analysis

--- gcli.js
+++ gcli.js
@@ -7,37 +7,46 @@
 
 exports.gcliHome = __dirname;
 
-var fs = require('fs');
-
 // It's tempting to use RequireJS from npm, however that would break
 // running GCLI in Firefox just by opening index.html
-exports.requirejs = require('./scripts/r.js');
-exports.requirejs.config({
-  nodeRequire: require,
-  paths: { 'text': 'scripts/text', 'i18n': 'scripts/i18n' },
-  packagePaths: {
-    'lib': [
-      { name: 'gcli', main: 'index', lib: '.' },
-      { name: 'test', main: 'index', lib: '.' },
-      { name: 'gclitest', main: 'index', lib: '.' },
-      { name: 'demo', main: 'index', lib: '.' },
-      { name: 'server', main: 'index', lib: '.' }
-    ]
-  }
+var requirejs = require('./scripts/r.js');
+requirejs.config({
+ nodeRequire: require,
+ paths: { 'text': 'scripts/text', 'i18n': 'scripts/i18n' },
+ packagePaths: {
+   'lib': [
+     { name: 'gcli', main: 'index', lib: '.' },
+     { name: 'test', main: 'index', lib: '.' },
+     { name: 'gclitest', main: 'index', lib: '.' },
+     { name: 'demo', main: 'index', lib: '.' },
+     { name: 'server', main: 'index', lib: '.' }
+   ]
+ }
 });
 
-exports.requirejs('gcli/index');
-var help = exports.requirejs('gcli/commands/help');
+exports.require = requirejs;
+
+exports.require('gcli/index');
+
+// Load the commands defined in Node modules
+require('./lib/server/commands/unamd').startup();
+require('./lib/server/commands/firefox').startup();
+// require('./lib/server/commands/git').startup();
+require('./lib/server/commands/make').startup();
+require('./lib/server/commands/standard').startup();
+require('./lib/server/commands/test').startup();
+
+// Load the commands defined in CommonJS modules
+var help = exports.require('gcli/commands/help');
 help.startup();
-exports.requirejs('gcli/commands/pref').startup();
-exports.requirejs('test/commands/test').startup();
-require('./lib/server/commands').startup();
+exports.require('gcli/commands/pref').startup();
 
-help.helpManHtml = fs.readFileSync(exports.gcliHome + '/lib/server/help_man.txt', 'utf8');
-help.helpListHtml = fs.readFileSync(exports.gcliHome + '/lib/server/help_list.txt', 'utf8');
+var fs = require('fs');
+help.helpManHtml = fs.readFileSync(exports.gcliHome + '/lib/server/commands/help_man.txt', 'utf8');
+help.helpListHtml = fs.readFileSync(exports.gcliHome + '/lib/server/commands/help_list.txt', 'utf8');
 
+// Serve or execute
 var server = require('./lib/server/index');
-
 if (process.argv.length < 3) {
   server.serve();
 }
