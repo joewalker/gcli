205149ef6c6c66d5e4332b5570159408a06bf26f
Joe Walker <jwalker@mozilla.com>
2012-06-28 14:51:34 +0100

    Bug 659686 (license): Update copyright year to 2012

--- gcli.js
+++ gcli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /*
- * Copyright 2011, Mozilla Foundation and contributors
+ * Copyright 2012, Mozilla Foundation and contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
