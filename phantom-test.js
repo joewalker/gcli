e188d9316eb4809c5db3b6080a565774449f221b
Joe Walker <jwalker@mozilla.com>
2013-01-18 12:10:30 +0000

    asynctypes-685526: Allow testing with PhantomJS
    In theory testing with PhantomJS should be better than JSDom because the
    former is a headless webkit rather than a JS emulation. Certainly so
    far there are way less exclusions. The one downside is that is can't be
    run from Node.
    
    Signed-off-by: Joe Walker <jwalker@mozilla.com>

---
+++ phantom-test.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+/*
+ * Copyright 2012, Mozilla Foundation and contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var page = require('webpage').create();
+
+var pageLoaded = function(status) {
+  setInterval(function() {
+    var complete = page.evaluate(function() {
+      return document.complete;
+    });
+
+    if (complete === true) {
+      phantom.exit();
+    }
+  }, 50);
+};
+
+page.onConsoleMessage = function() {
+  console.log.apply(console, arguments);
+};
+
+page.open('http://localhost:9999/localtest.html', pageLoaded);
